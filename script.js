f331b05650c0d09171a5554b0cbca2d03a9f880e
brandonstevenz2002 <brandonstevenvm@gmail.com>
2025-04-28 22:02:19 -0500

    Actualizacion de mi cv siguiendo la estructura del profesor

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     if (downloadBtn) {
         downloadBtn.addEventListener("click", function () {
             // Importar jsPDF y html2canvas
-            const { jsPDF } = window.jspdf;
+            const  element = document.body;
 
-            html2canvas(document.querySelector("body")).then(canvas => {
-                const imgData = canvas.toDataURL("image/png");
-                const pdf = new jsPDF();
+            const opt = {
+                margin: 0.5,
+                filename: 'mi_cv.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+            };
 
-                // Ajustar el tamaño de la imagen al tamaño del PDF
-                const pdfWidth = pdf.internal.pageSize.getWidth();
-                const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-                pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-                pdf.save("Mi_CV.pdf");
-            });
+            html2pdf().set(opt).from(element).save();
         });
     }
 });
